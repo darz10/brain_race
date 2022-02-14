9943e7cf6ef655243415aa2ba4846b8bfe4a698e
darz10 <you@example.com>
2022-02-14 21:08:15 +0500

    Change race/views.py

--- race/views.py
+++ race/views.py
@@ -20,46 +20,49 @@ async def get_main_data(current_user: User = Depends(get_current_user)):
         return ResponseStatus(
             success=False,
             status_code=400,
-            resposne="Error receiving information"
+            resposne="Error receiving information",
         )
 
+
 @router.get("/v1/choice-car")
-async def get_available_cars(
-    current_user: User = Depends(get_current_user)
-    ):
+# //TODO сделать из этого метода 2 эндпоинта для получения всех машин пользователя и для изменения текущей машины
+async def get_available_cars(current_user: User = Depends(get_current_user)):
     db = BrainRaceDB(settings.psql_conn)
     car = Car(BrainRaceDB=db)
     try:
-        user_cars = await car.get_avalible_user_cars(username=current_user["username"])
+        user_cars = await car.get_avalible_user_cars(
+            username=current_user["username"]
+        )
         return user_cars
     except Exception as e:
         print(e)
         return ResponseStatus(
             success=False,
             status_code=400,
-            resposne="Error receiving information"
+            resposne="Error receiving information",
         )
 
 
 @router.put("/v1/update-current-car")
 async def update_curr_car(
-    request_body: UpdCar,
-    current_user: User = Depends(get_current_user)
-    ):
+    request_body: UpdCar, current_user: User = Depends(get_current_user)
+):
     """Изменение выбора текущей машины"""
     db = BrainRaceDB(settings.psql_conn)
     car = Car(BrainRaceDB=db)
     try:
-        user_cars = await car.get_avalible_user_cars(username=current_user["username"])
+        user_cars = await car.get_avalible_user_cars(
+            username=current_user["username"]
+        )
         return ResponseStatus(
             success=truncate,
             status_code=200,
-            resposne="Current car updated successfully"
+            resposne="Current car updated successfully",
         )
     except Exception as e:
         print(e)
         return ResponseStatus(
             success=False,
             status_code=400,
-            resposne="Сurrent сar has not been updated."
+            resposne="Сurrent сar has not been updated.",
         )
